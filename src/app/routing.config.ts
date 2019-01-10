71a6ee6e6ba8483e875962bc4f4cd3a285ad0982
Luis Alberto <disconluis@gmail.com>
2019-01-10 12:21:00 +0100

    add mdbootstrap

--- src/app/routing.config.ts
+++ src/app/routing.config.ts
@@ -1,6 +1,11 @@
 import { Routes } from "@angular/router";
+import { HomeComponent } from './views/home/home.component';
+import { OfertasComponent } from './views/ofertas/ofertas.component';
+import { CartaComponent } from './views/carta/carta.component';
 
 
 export const routes: Routes = [
-  // {path: '', component: }
+  {path: '', component: HomeComponent},
+  {path: 'ofertas', component: OfertasComponent},
+  {path: 'carta', component: CartaComponent}
 ];

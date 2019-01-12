6820c5c6c61e330b6021e3c5cba7cf03b0250f53
Luis Alberto <disconluis@gmail.com>
2019-01-12 14:33:49 +0100

    fixed messenger's link

--- src/app/models/entrantes.ts
+++ src/app/models/entrantes.ts
@@ -1,23 +1,23 @@
 const entrantes = [
   {
-    title: 'Alitas de pollo',
-    img: 'assets/img/entrantes/entrantes-alitas.jpg',
-    category: 'Entrantes',
-    description:'Pequeña 6€, Mediana 10€, Grande 15€',
-    buttonText: 'Pedir'
-  },
-  {
-    title: 'Croquetas',
-    img: 'assets/img/entrantes/entrantes-croquetas.jpg',
-    category: 'Entrantes',
-    description:'Pequeña 6€, Mediana 10€, Grande 15€',
-    buttonText: 'Pedir'
-  },
-  {
     title: 'Empanadillas',
     img: 'assets/img/entrantes/entrantes-empanadillas.jpg',
     category: 'Entrantes',
     description:'Atún, Jamón York y Carne',
+    buttonText: 'Pedir'
+  },
+  {
+    title: 'Salteñas',
+    img: 'assets/img/entrantes/entrantes-empanadillas.jpg',
+    category: 'Entrantes',
+    description:'Masa dulce rellena de pollo',
+    buttonText: 'Pedir'
+  },
+  {
+    title: 'Crujientes',
+    img: 'assets/img/entrantes/entrantes-empanadillas.jpg',
+    category: 'Entrantes',
+    description:'Pollo crujiente: 4U.',
     buttonText: 'Pedir'
   },
   {
@@ -28,12 +28,54 @@ const entrantes = [
     buttonText: 'Pedir'
   },
   {
+    title: 'Alitas de pollo',
+    img: 'assets/img/entrantes/entrantes-alitas.jpg',
+    category: 'Entrantes',
+    description:'Pequeña 6€, Mediana 10€, Grande 15€',
+    buttonText: 'Pedir'
+  },
+  {
+    title: 'Nuggets',
+    img: 'assets/img/entrantes/entrantes-provolones.jpg',
+    category: 'Entrantes',
+    description:'Nuggets de pollo: 5U.',
+    buttonText: 'Pedir'
+  },
+  {
+    title: 'Fritas',
+    img: 'assets/img/entrantes/entrantes-provolones.jpg',
+    category: 'Entrantes',
+    description:'Patatas fritas',
+    buttonText: 'Pedir'
+  },
+  {
+    title: 'Bravas',
+    img: 'assets/img/entrantes/entrantes-provolones.jpg',
+    category: 'Entrantes',
+    description:'Patatas Bravas con all i oli',
+    buttonText: 'Pedir'
+  },
+  {
+    title: 'Croquetas',
+    img: 'assets/img/entrantes/entrantes-croquetas.jpg',
+    category: 'Entrantes',
+    description:'Pequeña 6€, Mediana 10€, Grande 15€',
+    buttonText: 'Pedir'
+  },
+  {
     title: 'Caprese',
     img: 'assets/img/entrantes/entrantes-caprese.jpg',
     category: 'Entrantes',
     description:'Ensalada Caprese con ingredientes naturales',
     buttonText: 'Pedir'
-  }
+  },
+  {
+    title: 'Pan de Ajo',
+    img: 'assets/img/entrantes/entrantes-provolones.jpg',
+    category: 'Entrantes',
+    description:'5 Unidades',
+    buttonText: 'Pedir'
+  },
 ]
 
 export default entrantes

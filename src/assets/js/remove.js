efa77d1d71238e6e6d6f64b1ba4524cd493ea453
Luis Alberto <disconluis@gmail.com>
2019-01-15 18:05:01 +0100

    add remove.js

---
+++ src/assets/js/remove.js
@@ -0,0 +1,10 @@
+//VARIABLES
+let hijo = document.getElementsByTagName('div')
+
+//FUNCIONES
+const inicio = () => {
+  hijo[44].style.display = 'none'
+}
+
+//EVENTOS
+window.addEventListener('load', inicio)

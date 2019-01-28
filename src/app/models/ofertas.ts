42efc18f3c32ea2fefa4d08b7446f7c934d6399a
Luis Alberto <disconluis@gmail.com>
2019-01-28 15:27:01 +0100

    cambiada las ofertas

--- src/app/models/ofertas.ts
+++ src/app/models/ofertas.ts
@@ -1,15 +1,15 @@
 const ofertas = [
   {
     image: 'assets/img/ofertas/little-pizza.jpg',
-    title: 'PIZZA PERSONAL Y BEBIDA',
+    title: 'PIZZA PERSONAL Y REFRESCO lata',
     precio: '5',
-    description: 'No incluye 4 Estaciones'
+    description: 'Lunes a Jueves. No incluye 4 Estaciones'
   },
   {
     image: 'assets/img/ofertas/dos-pizzas.jpg',
-    title: 'DOS PIZZAS MEDIANAS Y BEBIDA',
+    title: 'DOS PIZZAS MEDIANAS Y REFRESCO 1 litro',
     precio: '20',
-    description: 'Pizzas especiales 2€ más'
+    description: 'No incluye pizzas especiales'
   },
   {
     image: 'assets/img/ofertas/tres-pizzas.jpg',
@@ -22,12 +22,6 @@ const ofertas = [
     title: 'PIZZA MEDIANA',
     precio: '8',
     description: 'Lunes a jueves, a recoger en el local'
-  },
-  {
-    image: 'assets/img/ofertas/menu-dos.jpg',
-    title: 'MENU PARA DOS',
-    precio: '16,50',
-    description: 'incluye: Pizza mediana, entrante, bebida y postre'
   },
   {
     image: 'assets/img/ofertas/menu-tres.jpg',

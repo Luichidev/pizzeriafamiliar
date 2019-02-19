16be8f6b93ad41a03940313094092cb72b6048b8
Luis Alberto <disconluis@gmail.com>
2019-02-19 15:12:49 +0100

    agregado cambios de parte de la empresa

--- src/app/models/ofertas.ts
+++ src/app/models/ofertas.ts
@@ -1,33 +1,45 @@
 const ofertas = [
   {
     image: 'assets/img/ofertas/little-pizza.jpg',
-    title: 'PIZZA PERSONAL Y REFRESCO lata',
+    title: 'PIZZA PERSONAL',
     precio: '5',
-    description: 'Lunes a Jueves. No incluye 4 Estaciones'
+    description: 'más refresco de lata de Domingo a Jueves, ver más abajo las opciones',
+    textLittle: true
   },
   {
     image: 'assets/img/ofertas/dos-pizzas.jpg',
-    title: 'DOS PIZZAS MEDIANAS Y REFRESCO 1 litro',
+    title: 'DOS PIZZAS MEDIANAS',
     precio: '20',
-    description: 'No incluye pizzas especiales'
+    description: 'más Bebida de 1 litro, Incluye pizzas especiales',
+    textLittle: false
   },
   {
     image: 'assets/img/ofertas/tres-pizzas.jpg',
     title: 'TRES PIZZAS MEDIANAS',
     precio: '25',
-    description: 'No incluye pizzas especiales'
+    description: 'Domingo a jueves, No incluye pizzas especiales',
+    textLittle: true
   },
   {
     image: 'assets/img/ofertas/mediana-pizza.jpg',
     title: 'PIZZA MEDIANA',
     precio: '8',
-    description: 'Lunes a jueves, a recoger en el local'
+    description: 'Lunes a jueves, a recoger en el local',
+    textLittle: true
+  },
+  {
+    image: 'assets/img/ofertas/menu-dos.jpg',
+    title: 'MENU PARA DOS',
+    precio: '15',
+    description: 'Pizza mediana, entrante y 2 latas de refresco',
+    textLittle: false
   },
   {
     image: 'assets/img/ofertas/menu-tres.jpg',
     title: 'MENU PARA TRES',
     precio: '18,50',
-    description: 'Pizza grande, entrante y bebida'
+    description: 'Pizza grande, entrante y Refresco de 1 litro',
+    textLittle: false
   }
 ]
 

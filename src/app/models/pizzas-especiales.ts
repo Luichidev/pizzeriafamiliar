c60e2b1e6410af424e767672be05d6b2cd0631da
@LuisArana <disconluis@gmail.com>
2020-09-30 13:59:22 +0200

    se actulizo los ingredientes de las pizzas

--- src/app/models/pizzas-especiales.ts
+++ src/app/models/pizzas-especiales.ts
@@ -1,53 +1,61 @@
 const especiales = [
   {
-    title: 'Jamon de Serrano',
-    img: 'assets/img/pizzas/pizza-jamon-serrano.jpg',
-    category: 'Especiales',
-    description:'Pequeña 8€, Mediana 12€, Grande 18€',
-    buttonText: 'Pedir'
+    title: "Jamon de Serrano",
+    img: "assets/img/pizzas/pizza-jamon-serrano.jpg",
+    category: "Especiales",
+    ingredients: "Mozzarella y jamón serrano.",
+    description: "Pequeña 8€, Mediana 12€, Grande 18€",
+    buttonText: "Pedir",
   },
   {
-    title: 'Chorizo',
-    img: 'assets/img/pizzas/pizza-chorizo.jpg',
-    category: 'Especiales',
-    description:'Pequeña 8€, Mediana 12€, Grande 18€',
-    buttonText: 'Pedir'
+    title: "Chorizo",
+    img: "assets/img/pizzas/pizza-chorizo.jpg",
+    category: "Especiales",
+    ingredients: "Mozzarella y chorizo.",
+    description: "Pequeña 8€, Mediana 12€, Grande 18€",
+    buttonText: "Pedir",
   },
   {
-    title: 'Peperoni',
-    img: 'assets/img/pizzas/pizza-peperoni.jpg',
-    category: 'Especiales',
-    description:'Pequeña 8€, Mediana 12€, Grande 18€',
-    buttonText: 'Pedir'
+    title: "Peperoni",
+    img: "assets/img/pizzas/pizza-peperoni.jpg",
+    category: "Especiales",
+    ingredients: "Peperoni picante.",
+    description: "Pequeña 8€, Mediana 12€, Grande 18€",
+    buttonText: "Pedir",
   },
   {
-    title: 'Chef',
-    img: 'assets/img/pizzas/pizza-chef.jpg',
-    category: 'Especiales',
-    description:'Pequeña 8€, Mediana 12€, Grande 18€',
-    buttonText: 'Pedir'
+    title: "Chef",
+    img: "assets/img/pizzas/pizza-chef.jpg",
+    category: "Especiales",
+    ingredients: "Gambas, champiñones, jamón, bacon y aceitunas.",
+    description: "Pequeña 8€, Mediana 12€, Grande 18€",
+    buttonText: "Pedir",
   },
   {
-    title: 'Salchipapa',
-    img: 'assets/img/pizzas/pizza-salchipapa.jpg',
-    category: 'Tradicionales',
-    description:'Pequeña 6€, Mediana 10€, Grande 15€',
-    buttonText: 'Pedir'
+    title: "Salchipapa",
+    img: "assets/img/pizzas/pizza-salchipapa.jpg",
+    category: "Especiales",
+    ingredients: "Bacon, jamón, franfurt, papatas paja.",
+    description: "Pequeña 6€, Mediana 10€, Grande 15€",
+    buttonText: "Pedir",
   },
   {
-    title: 'De la Casa',
-    img: 'assets/img/pizzas/pizza-delacasa.jpg',
-    category: 'Especiales',
-    description:'Pequeña 8€, Mediana 12€, Grande 18€',
-    buttonText: 'Pedir'
+    title: "De la Casa",
+    img: "assets/img/pizzas/pizza-delacasa.jpg",
+    category: "Especiales",
+    ingredients:
+      "Jamón, esparragos, alcachofas, champiñones, tomate, rulo de Cabra.",
+    description: "Pequeña 8€, Mediana 12€, Grande 18€",
+    buttonText: "Pedir",
   },
   {
-    title: 'Calzones',
-    img: 'assets/img/pizzas/pizza-calzone.jpg',
-    category: 'Especiales',
-    description:'Pequeña 8€, Mediana 12€, Grande 18€',
-    buttonText: 'Pedir'
-  }
-]
+    title: "Calzones",
+    img: "assets/img/pizzas/pizza-calzone.jpg",
+    category: "Especiales",
+    ingredients: "A elegir.",
+    description: "Pequeña 8€, Mediana 12€, Grande 18€",
+    buttonText: "Pedir",
+  },
+];
 
-export default especiales
+export default especiales;
